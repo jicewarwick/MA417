decb0d12f8d5b7799edefe97674c285a8f5edca5
Ce Ji <jicewarwick@gmail.com>
2014-03-30 20:53:30 +0100

    improve speed with getnumbers function

--- NormalBoxMullerSampler.cc
+++ NormalBoxMullerSampler.cc
@@ -10,14 +10,15 @@ NormalBoxMullerSampler::~NormalBoxMullerSampler(void){}
 std::vector<double> NormalBoxMullerSampler::getnumbers(int n){
 	std::vector<double> ret;
 	UniformSampler *U = new UniformSampler();
+	double U1 = 0, U2 = 0, R = 0, theta = 0;
 	int rep = ceil(n / 2.0);
 	for (int i = 0; i < rep; ++i) {
-		double U1 = U->getnumber();
-		double U2 = U->getnumber();
-		double R = -2 * log(U1);
-		double theta = 2 * PI * U2;
-		ret.push_back( sqrt(R) * cos(theta));
-		ret.push_back( sqrt(R) * sin(theta));
+		U1 = U->getnumber();
+		U2 = U->getnumber();
+		R = -2 * log(U1);
+		theta = 2 * PI * U2;
+		ret.push_back( sqrt(R) * cos(theta) );
+		ret.push_back( sqrt(R) * sin(theta) );
 	}
 	if (n % 2 == 1) {
 		ret.pop_back();
