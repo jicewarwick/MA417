9ed07cf0fcffd1ae5e6618e0d6ad4d1b0a591249
Ce Ji <jicewarwick@gmail.com>
2014-03-30 20:52:41 +0100

    Generate all random number in one go

--- MonteCarloSimulation.cc
+++ MonteCarloSimulation.cc
@@ -1,6 +1,4 @@
 #include "MonteCarloSimulation.h"
-#include "unistd.h"
-
 
 MonteCarloSimulation::MonteCarloSimulation (double (*f) (double), Sampler* randgen, int n){
 	f_ = f;
@@ -12,13 +10,12 @@ MonteCarloSimulation::~MonteCarloSimulation(){}
 double MonteCarloSimulation::simulate(void){
 	double ret = 0;
 	double sum = 0;
-	int i;
+	std::vector<double> v;
+	v = randgen_->getnumbers(n_);
 
-	for (i = 0; i < n_; ++i) {
-		sum += f_(randgen_->getnumber());
-		//usleep(100);
+	for (int i = 0; i < n_; ++i) {
+		sum += f_(v[i]);
 	}
-
 	ret = sum / (double)n_;
 
 	return ret;
