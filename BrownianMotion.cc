e070681c690265d2d40c940a512167ebffe1586c
Ce Ji <jicewarwick@gmail.com>
2014-04-03 13:22:40 +0100

    clean up, fix ambiguous problem with the default constructor when no arguments are provided

--- BrownianMotion.cc
+++ BrownianMotion.cc
@@ -3,37 +3,20 @@
 #include <cmath>
 
 BrownianMotion::BrownianMotion(void){
-	mean_ = 0;
-	sd_ = 1;
+	BrownianMotion(0,1);
 }
 
 BrownianMotion::BrownianMotion(double mean, double sigma){
 	mean_ = mean;
 	sd_ = sigma;
+	NormalAccSampler norm_;
 }
 
 BrownianMotion::~BrownianMotion(){ }
 
-double BrownianMotion::getmean(){
-	return mean_;
-}
-
-double BrownianMotion::getsd(void){
-	return sd_;
-}
-
-void BrownianMotion::setmean(double mean){
-	mean_ = mean;
-}
-
-void BrownianMotion::setsd(double sd){
-	sd_ = sd;
-}
-
 double BrownianMotion::getnumber(){
 	double ret = 0;
-	NormalAccSampler *norm = new NormalAccSampler();
-	ret = norm->getnumber() * sd_;
+	ret = norm_.getnumber() * sd_;
 
 	return ret;
 }
