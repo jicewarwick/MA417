53cbec8db3b35e44b2b43c14730d736a85b528a0
Ce Ji <jicewarwick@gmail.com>
2014-04-03 13:22:06 +0100

    BrownianMotion.cc

--- BrownianMotion.h
+++ BrownianMotion.h
@@ -2,25 +2,28 @@
 #define BROWNIANMOTION_H_AMT9LZG3
 
 #include "Sampler.h"
+#include "NormalAccSampler.h"
 #include <vector>
 
 class BrownianMotion : public Sampler {
 public:
 	BrownianMotion (void);
-	BrownianMotion (double mean = 0, double sigma = 1);
+	BrownianMotion (double, double);
 	virtual ~BrownianMotion ();
 
-	double getmean(void);
-	double getsd(void);
+	double getmean(void) const {return mean_;}
+	double getsd(void) const {return sd_;}
 
-	void setmean(double);
-	void setsd(double);
+	void setmean(double mean){mean_ = mean;}
+	void setsd(double sd){sd_ = sd;}
 
 	virtual double getnumber();
+	//std::vector<double> getnumbers(int);
 
 private:
 	double mean_;
 	double sd_;
+	NormalAccSampler norm_;
 };
 
 
