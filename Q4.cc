6a21c646f1dc631344d2eef225ea55a0ebaebc7b
Ce Ji <jicewarwick@gmail.com>
2014-04-11 16:58:39 +0100

    Finish Worksheet 4

---
+++ Q4.cc
@@ -0,0 +1,28 @@
+#include "BrownianMotion.h"
+#include "GeometricBrownianMotion.h"
+#include <iostream>
+#include <cmath>
+#include "dataExport.h"
+
+int main(int argc, char *argv[]) {
+	double X0 = 2;
+	double mu = 1;
+	double sigma = 1;
+	double T = 3;
+	int N = 10000;
+
+
+	double b_sample_path[N+1];
+	double gb_sample_path[N+1];
+
+	BrownianMotion B(X0, mu, sigma);
+	B.get_samplepath(T, N, X0, b_sample_path);
+
+	GeometricBrownianMotion GB(X0, mu, sigma);
+	GB.get_samplepath(T, N, X0, gb_sample_path);
+
+	export_to_file(b_sample_path, N+1, "BM.dat");
+	export_to_file(gb_sample_path, N+1, "GBM.dat");
+	
+	return 0;
+}
